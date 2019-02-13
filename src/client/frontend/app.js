d002836743f309a52b56b8ff1fbf52f8931bd254
Matthias Busenhart <busenham@student.ethz.ch>
2019-02-13 17:17:00 +0100

    Update to frontend, first version now done (without ETH-connection)

--- src/client/frontend/app.js
+++ src/client/frontend/app.js
@@ -1,7 +1,22 @@
 var express = require('express');
-//const Web3 = require('web3');
-//const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
+// const Web3 = require('web3');
+// const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
 const sqlite3 = require('sqlite3').verbose();
+
+
+function hasNewBill() {
+  // TODO: do this
+  return true;
+}
+
+function getBillData() {
+  return {
+    energy_needed: 1234,
+    tokens_earned: 10,
+    price_per_unit: 10
+  };
+}
+
 
 let db = new sqlite3.Database('../database.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
@@ -18,16 +33,25 @@ app.use("/js", express.static(__dirname + '/js'));
 app.set('view engine', 'pug');
 
 app.route('/').get(function(req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
+  res.render('index');
 });
+
+app.route('/bill').get(function(req, res) {
+  if(hasNewBill()) {
+    res.render('bill',getBillData());
+  } else {
+    res.render('no_bill');
+  }
+})
 
 app.route('/api').get(function(req, res) {
   var params = req.query;
   var returnData = [];
-  db.all('SELECT time,data FROM sensor_data WHERE time<=\''+params.end+'\' AND time>=\''+params.begin+'\'', (err, rows) => {
+  db.all('SELECT time, data FROM sensor_data WHERE time<=\''+params.end+'\' AND time>=\''+params.begin+'\'', (err, rows) => {
     if (err) {
       console.error(err.message);
     }
+    console.log("Serving rows: " + rows.length);
     res.send(rows);
   });
 });
