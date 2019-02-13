c2445f9d278f063214b0525a68e78688dcda8299
Matthias Busenhart <busenham@student.ethz.ch>
2019-02-13 11:13:55 +0100

    Add Fond contract

--- src/smartcontracts/contracts/Fond.sol
+++ src/smartcontracts/contracts/Fond.sol
@@ -1,4 +1,26 @@
+pragma solidity >=0.4.0 <0.6.0;
 contract Fond {
-  mappable(address => uint)
-  
+  mapping(address => uint) investors; // how much did each investors send to me?
+  address[] invs;
+
+  address public billContractAddress; // stores address of bill contract
+
+  constructor(address billContract) public{
+    billContractAddress = billContract;
+  }
+
+  function receiveTokens(address investor, uint amount) public {
+    require(msg.sender == billContractAddress);
+    investors[investor] += amount;
+    invs.push(investor);
+  }
+
+  function releaseFond() public {
+    require(msg.sender == billContractAddress);
+    // reset all tokens invested
+    for (uint i = 0; i < invs.length; i++)
+      investors[invs[i]] = 0;
+    delete invs;
+  }
+
 }
