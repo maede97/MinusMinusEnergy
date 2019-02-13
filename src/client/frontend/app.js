e32eaf53746dbbef29003c23439462b56099f480
Matthias Busenhart <busenham@student.ethz.ch>
2019-02-13 18:13:51 +0100

    Save for merge

--- src/client/frontend/app.js
+++ src/client/frontend/app.js
@@ -1,8 +1,39 @@
 var express = require('express');
 const Web3 = require('web3');
-const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
-const sqlite3 = require('sqlite3').verbose();
+var util = require('ethereumjs-util');
+var tx = require('ethereumjs-tx');
+var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/1de4b23aea044238ab6c8500d2420f87'));
 
+var address = '0x22C2FD79de9A389dF193B0819D8CB6DB81B37734';
+var key = '6f4d49b5d4d00d7454fa7c3468c90a108795521b31803a2d1d47ca6094e86f03';
+
+var privateKey = new Buffer(key, 'hex');
+var txParams = {
+  nonce: '0x1e4',
+  gasPrice: '0x09184e72a000',
+  gasLimit: '0x271000',
+  to: '0x6f687DF4B652d7f2A69B4500A478E830d096EebA',
+  value: '0x01',
+  data: '0x0',
+  // EIP 155 chainId - mainnet: 1, ropsten: 3
+  chainId: 3
+}
+
+var transaction = new tx(txParams);
+transaction.sign(privateKey);
+
+var serializedTx = transaction.serialize().toString('hex');
+
+// web3.eth.sendSignedTransaction('0x'+serializedTx,function(err,result){
+//   if(err) {
+//       console.log(err);
+//   } else {
+//       console.log("all good");
+//       console.log(result);
+//   }
+// });
+
+const sqlite3 = require('sqlite3').verbose();
 
 function hasNewBill() {
   // TODO: do this
@@ -10,6 +41,7 @@ function hasNewBill() {
 }
 
 function getBillData() {
+  // TODO
   return {
     energy_needed: 1234,
     tokens_earned: 10,
