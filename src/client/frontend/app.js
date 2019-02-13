a051a1c5b339e108b2779044f649075059f47fd3
Matthias Busenhart <busenham@student.ethz.ch>
2019-02-13 17:18:57 +0100

    small update

--- src/client/frontend/app.js
+++ src/client/frontend/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
-// const Web3 = require('web3');
-// const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
+const Web3 = require('web3');
+const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
 const sqlite3 = require('sqlite3').verbose();
 
 
--- src/client/frontend/js/billCalc.js
+++ src/client/frontend/js/billCalc.js
@@ -1,10 +1,3 @@
-// $('#slider').change(function(){
-//   $('#bill').html(parseFloat($('#tokens').html()) * parseFloat($('#slider').attr('value')) / 100.0);
-//   $('#fond').html(parseFloat($('#tokens').html()) * (1.0-(parseFloat($('#slider').attr('value')) / 100.0)));
-//   console.log($('#slider').attr('value'));
-// });
-
-
 $(document).on('input', '#slider', function() {
   $('#bill').html(parseFloat($('#tokens').html()) * parseFloat($('#slider').val()) / 100.0);
   $('#fond').html(Math.round((parseFloat($('#tokens').html()) * (1.0-(parseFloat($('#slider').val()) / 100.0)))*100)/100.0);
