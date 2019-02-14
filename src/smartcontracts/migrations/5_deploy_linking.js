77cf1c396d60e4306a8f17c8f22aa24de132c1be
Philip Wiese <philip.wiese@maketec.ch>
2019-02-14 12:50:09 +0100

    Nicer code

--- src/smartcontracts/migrations/5_deploy_linking.js
+++ src/smartcontracts/migrations/5_deploy_linking.js
@@ -2,19 +2,13 @@ const MMETokenContract = artifacts.require("MMEToken");
 const BillContract = artifacts.require("Bill");
 const FondContract = artifacts.require("Fond");
 
-module.exports = function(deployer) {
-  // Deploy BillContract, then deploy FondContract, passing in BillContract address
-  MMETokenContract.deployed()
-    .then(function(MMETokenInstance) {
-    FondContract.deployed()
-      .then(function(FondContractInstance) {
-        BillContract.deployed()
-          .then(function(BillContractInstance) {
-            MMETokenInstance.setBillContract(BillContractInstance.address);
-            MMETokenInstance.setFondContract(FondContractInstance.address);
-            BillContract.setFondContract(FondContractInstance.address);
-            BillContract.setMMETokenContract(MMETokenInstance.address);
-        });
-    });
-  })
+module.exports = async function(deployer) {
+  const BillContractInstance = await BillContract.deployed();
+  const MMETokenInstance = await MMETokenContract.deployed();
+
+  MMETokenInstance.setBillContract(BillContract.address);
+  MMETokenInstance.setFondContract(FondContract.address);
+  BillContractInstance.setFondContract(FondContract.address);
+  BillContractInstance.setMMETokenContract(MMETokenInstance.address);
+
 };
