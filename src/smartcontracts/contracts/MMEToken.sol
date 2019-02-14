ae9c747d0d24320c1464fcfa5d933b0456201b24
Philip Wiese <philip.wiese@maketec.ch>
2019-02-14 01:12:58 +0100

    Working verification of data

--- src/smartcontracts/contracts/MMEToken.sol
+++ src/smartcontracts/contracts/MMEToken.sol
@@ -8,40 +8,45 @@ import { ECDSA } from "../libraries/ECDSA.sol";
 contract MMEToken is ERC20Detailed, ERC20Mintable {
   using ECDSA for bytes32;
 
-  event Debug(address sender ,address signer);
-  event Hash(bytes32 hash);
+  event Error(string message);
 
   // Prevent Replay Attacks for claiming tokens
   mapping(address => mapping(uint256 => bool)) seenNonces;
 
-  constructor() ERC20Detailed('MinusMinusEnergy Coin', 'MME', 2) public {
+  constructor()
+    ERC20Detailed('MinusMinusEnergy Coin', 'MME', 2)
+    public
+    {
+      addMinter(address(this));
+
   }
 
   // Planned: Data Signature verificatioin with EEC
   // https://yos.io/2018/11/16/ethereum-signatures/
-  function claimToken(address sender, uint256 amount, uint256 nonce, bytes memory signature) public {
+  function claimToken(address sender, uint256 amount, uint256 nonce, bytes memory signature) public  {
 
     // This recreates the message hash that was signed on the client.
     bytes32 hash = keccak256(abi.encodePacked(sender, amount, nonce));
     bytes32 messageHash = hash.toEthSignedMessageHash();
 
-    emit Hash(messageHash);
-
     // Verify that the message's signer is the owner of the order
     address signer = messageHash.recover(signature);
-    emit Debug(sender, signer);
-
-    //require(signer == sender);
+    //require(signer == sender, "Invalid signature");
 
     // Prevent ReplayAttacks
-    //require(!seenNonces[signer][nonce]);
+    require(!seenNonces[signer][nonce], "Tokens already claimed");
     seenNonces[signer][nonce] = true;
 
     // Reward Token
-    mint(signer, amount);
-    emit Transfer(address(0x0), signer, amount);
+    //SCheis fucking z8iiile
+    _mint(signer, amount);
   }
 
+  function payBill() public {
+      // Call to BillContract
+  }
 
+  function setFondContract() public {
 
+  }
 }
--- src/smartcontracts/migrations/2_deploy_contracts.js
+++ src/smartcontracts/migrations/2_deploy_contracts.js
@@ -4,9 +4,11 @@ const FondContract = artifacts.require("Fond");
 
 module.exports = function(deployer) {
   // Deploy BillContract, then deploy FondContract, passing in BillContract address
-  deployer.deploy(MMETokenContract);
-  deployer.deploy(BillContract).then(function() {
-      return deployer.deploy(FondContract, BillContract.address);
+  deployer.deploy(MMETokenContract).then(function() {
+    deployer.deploy(BillContract, MMETokenContract.address).then(function() {
+        return deployer.deploy(FondContract, BillContract.address);
+    });
   });
+
 
 };
